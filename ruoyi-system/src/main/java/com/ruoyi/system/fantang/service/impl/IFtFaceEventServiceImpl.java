e69d57e1a1abcd92bc7a7a24f780d63c7d337c0d
czx <28353131@qq.com>
2021-01-11 11:48:14 +0800

    引入人脸识别部分代码

---
+++ ruoyi-system/src/main/java/com/ruoyi/system/fantang/service/impl/IFtFaceEventServiceImpl.java
@@ -0,0 +1,16 @@
+package com.ruoyi.system.fantang.service.impl;
+
+import com.ruoyi.system.fantang.domain.FtFaceEventDao;
+import com.ruoyi.system.fantang.service.IFtFaceEventService;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+
+@Service
+public class IFtFaceEventServiceImpl implements IFtFaceEventService {
+    @Override
+    public List<FtFaceEventDao> selectByMap(HashMap<String, Object> map) {
+        return null;
+    }
+}
