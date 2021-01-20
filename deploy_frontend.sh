25ada6fdb9a5ac866f9d79e8b9a42bd032bd2d75
mactj <tottimctj@163.com>
2021-01-20 15:31:14 +0800

    部署放开编译

--- deploy_frontend.sh
+++ deploy_frontend.sh
@@ -2,7 +2,7 @@
 
 echo "==========打包========="
 cd ruoyi-ui
-#npm run build:prod
+npm run build:prod
 echo "==========上传服务器========="
 scp -r dist/* root@62.234.115.161:/data/mall/
 echo "==========部署完成========="
