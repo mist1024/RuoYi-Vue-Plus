3eb2c7981350d23fe3e1bbfb6c16026e74c7a2c9
yinguofeng <11544672@qq.com>
2021-03-13 11:43:53 +0800

    公司名称修改

--- jiajiakang-common/src/main/java/com/jiajiakang/common/config/RuoYiConfig.java
+++ jiajiakang-common/src/main/java/com/jiajiakang/common/config/RuoYiConfig.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
  * @author ruoyi
  */
 @Component
-@ConfigurationProperties(prefix = "ruoyi")
+@ConfigurationProperties(prefix = "jiajiakang")
 public class RuoYiConfig
 {
     /** 项目名称 */
--- ruoyi-demo/src/main/java/com/ruoyi/demo/controller/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.jiajiakang.demo.controller;
