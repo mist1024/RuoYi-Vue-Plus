1bae0ca3cad65032602c84a2f23d018eb1dafded
during <during@inrice.com>
2021-06-11 17:15:08 +0800

    feature: 增加了雪花id配置文件，还没有测试

--- ruoyi-framework/src/main/java/com/ruoyi/framework/config/JacksonConfig.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/config/JacksonConfig.java
@@ -4,19 +4,26 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
+/**
+ * Ming LI
+ * 当Mybatis plus设置为雪花ID时，使用此类，会把所有数字返回变为字符串返回适配前端Long型失真问题
+ */
 @Configuration
 public class JacksonConfig {
 
     @Bean
     @Primary
     @ConditionalOnMissingBean(ObjectMapper.class)
-    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder)
+	@ConditionalOnProperty(value = "mybatis-plus.global-config.dbConfig.idType",havingValue = "ASSIGN_ID")
+	public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder)
     {
+
         ObjectMapper objectMapper = builder.createXmlMapper(false).build();
 
         // 全局配置序列化返回 JSON 处理
