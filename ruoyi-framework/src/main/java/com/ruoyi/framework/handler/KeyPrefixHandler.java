fa31d232264004949ce11fc37692f9bfbc93f3bd
抓蛙师 <770492966@qq.com>
2022-07-14 19:05:12 +0800

    redission处理增加前缀

---
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/handler/KeyPrefixHandler.java
@@ -0,0 +1,43 @@
+package com.ruoyi.framework.handler;
+
+import com.ruoyi.common.utils.StringUtils;
+import org.redisson.api.NameMapper;
+
+/*
+ * redis缓存key前缀处理
+ * @author ye
+ * @create 2022/7/14 17:44
+ */
+public class KeyPrefixHandler implements NameMapper {
+  
+  private final String keyPrefix;
+  
+  public KeyPrefixHandler(String keyPrefix) {
+    this.keyPrefix = keyPrefix + ":";
+  }
+  
+  //增加前缀
+  @Override
+  public String map(String name) {
+    if (StringUtils.isBlank(name)) {
+      return null;
+    }
+    if (!name.startsWith(keyPrefix)) {
+      return keyPrefix + name;
+    } else {
+      return name;
+    }
+  }
+  
+  //去除前缀
+  @Override
+  public String unmap(String name) {
+    if (StringUtils.isBlank(name)) {
+      return null;
+    }
+    if (name.startsWith(keyPrefix)) {
+      return name.substring(keyPrefix.length());
+    }
+    return name;
+  }
+}
