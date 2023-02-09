a84b62a0c1e3e74d244640601e87467b4867fce5
疯狂的狮子li <15040126243@163.com>
2023-02-09 17:52:59 +0800

    fix 修复 动态租户判断逻辑写反

--- ruoyi-common/ruoyi-common-mybatis/src/main/java/com/ruoyi/common/mybatis/handler/PlusTenantLineHandler.java
+++ ruoyi-common/ruoyi-common-mybatis/src/main/java/com/ruoyi/common/mybatis/handler/PlusTenantLineHandler.java
@@ -29,7 +29,7 @@ public class PlusTenantLineHandler implements TenantLineHandler {
             return new NullValue();
         }
         String dynamicTenantId = TenantHelper.getDynamic();
-        if (StringUtils.isBlank(dynamicTenantId)) {
+        if (StringUtils.isNotBlank(dynamicTenantId)) {
             // 返回动态租户
             return new LongValue(dynamicTenantId);
         }
