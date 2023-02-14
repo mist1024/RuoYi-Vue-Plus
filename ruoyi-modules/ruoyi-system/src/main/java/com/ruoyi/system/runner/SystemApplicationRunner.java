04ededaf50589923336dcce962db7b4283d772b5
疯狂的狮子li <15040126243@163.com>
2023-02-14 17:38:42 +0800

    update 重构 项目初始化数据改为懒加载 不提供热加载

--- ruoyi-modules/ruoyi-system/src/main/java/com/ruoyi/system/runner/SystemApplicationRunner.java
+++ ruoyi-modules/ruoyi-system/src/main/java/com/ruoyi/system/runner/SystemApplicationRunner.java
@@ -1,9 +1,5 @@
 package com.ruoyi.system.runner;
 
-import com.ruoyi.common.core.config.RuoYiConfig;
-import com.ruoyi.common.tenant.helper.TenantHelper;
-import com.ruoyi.system.service.ISysConfigService;
-import com.ruoyi.system.service.ISysDictTypeService;
 import com.ruoyi.system.service.ISysOssConfigService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,25 +17,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class SystemApplicationRunner implements ApplicationRunner {
 
-    private final RuoYiConfig ruoyiConfig;
-    private final ISysConfigService configService;
-    private final ISysDictTypeService dictTypeService;
     private final ISysOssConfigService ossConfigService;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        TenantHelper.enableIgnore();
-//        ossConfigService.init();
+        ossConfigService.init();
         log.info("初始化OSS配置成功");
-        if (ruoyiConfig.isCacheLazy()) {
-            TenantHelper.disableIgnore();
-            return;
-        }
-        configService.loadingConfigCache();
-        log.info("加载参数缓存数据成功");
-        dictTypeService.loadingDictCache();
-        log.info("加载字典缓存数据成功");
-        TenantHelper.disableIgnore();
     }
 
 }
