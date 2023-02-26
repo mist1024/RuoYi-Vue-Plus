ab1299cbb297f7eca17e882582826d953a1376cc
wdtt <boasad@163.com>
2023-02-26 18:58:13 +0800

    限流注解的自定义注解以及使用SpringEl表达式动态定义Key

--- ruoyi-framework/src/main/java/com/ruoyi/framework/aspectj/RateLimiterAspect.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/aspectj/RateLimiterAspect.java
@@ -5,6 +5,7 @@ import com.ruoyi.common.enums.LimitType;
 import com.ruoyi.common.exception.ServiceException;
 import com.ruoyi.common.utils.MessageUtils;
 import com.ruoyi.common.utils.ServletUtils;
+import com.ruoyi.common.utils.StringUtils;
 import com.ruoyi.common.utils.redis.RedisUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
@@ -12,6 +13,10 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.redisson.api.RateType;
+import org.springframework.core.DefaultParameterNameDiscoverer;
+import org.springframework.expression.common.TemplateParserContext;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
@@ -38,18 +43,54 @@ public class RateLimiterAspect {
             }
             long number = RedisUtils.rateLimiter(combineKey, rateType, count, time);
             if (number == -1) {
-                throw new ServiceException(MessageUtils.message("rate.limiter.message"));
+                String message = rateLimiter.message();
+                if (StringUtils.startsWith(message, "{") && StringUtils.endsWith(message, "}")) {
+                    message = MessageUtils.message(StringUtils.substring(message, 1, message.length() - 1));
+                }
+                throw new ServiceException(message);
             }
             log.info("限制令牌 => {}, 剩余令牌 => {}, 缓存key => '{}'", count, number, combineKey);
-        } catch (ServiceException e) {
-            throw e;
         } catch (Exception e) {
-            throw new RuntimeException("服务器限流异常，请稍候再试");
+            if(e instanceof ServiceException){
+                throw e;
+            }else{
+                throw new RuntimeException("服务器限流异常，请稍候再试");
+            }
         }
     }
 
     public String getCombineKey(RateLimiter rateLimiter, JoinPoint point) {
-        StringBuilder stringBuffer = new StringBuilder(rateLimiter.key());
+        String key = rateLimiter.key();
+        //获取方法(通过方法签名来获取)
+        MethodSignature signature = (MethodSignature) point.getSignature();
+        Method method = signature.getMethod();
+        Class<?> targetClass = method.getDeclaringClass();
+        if(StringUtils.startsWith(key,"#{") && StringUtils.endsWith(key,"}")){
+            //获取参数值
+            Object[] args = point.getArgs();
+            //获取方法上参数的名称
+            /**
+             * 这里的DefaultParameterNameDiscoverers是Spring自带的解析器
+             * 在SpringMvc获取参数的过程中也有用到
+             */
+            String[] parameterNames = new DefaultParameterNameDiscoverer().getParameterNames(method);
+            //使用Spring的el表达式解析
+            SpelExpressionParser parser = new SpelExpressionParser();
+            //注入到上下文对象中进行解析
+            StandardEvaluationContext context = new StandardEvaluationContext();
+            for (int i = 0; i < parameterNames.length; i++) {
+                context.setVariable(parameterNames[i],args[i]);
+            }
+            //定义解析模版
+            TemplateParserContext parser_content = new TemplateParserContext();
+            //解析返回给key
+            try {
+                key = parser.parseExpression(key,parser_content).getValue(context,String.class) + "";
+            }catch (Exception e){
+                throw new ServiceException("限流key解析异常!请联系管理员!");
+            }
+        }
+        StringBuilder stringBuffer = new StringBuilder();
         if (rateLimiter.limitType() == LimitType.IP) {
             // 获取请求ip
             stringBuffer.append(ServletUtils.getClientIP()).append("-");
@@ -57,10 +98,8 @@ public class RateLimiterAspect {
             // 获取客户端实例id
             stringBuffer.append(RedisUtils.getClient().getId()).append("-");
         }
-        MethodSignature signature = (MethodSignature) point.getSignature();
-        Method method = signature.getMethod();
-        Class<?> targetClass = method.getDeclaringClass();
         stringBuffer.append(targetClass.getName()).append("-").append(method.getName());
+        stringBuffer.append(":").append(key);
         return stringBuffer.toString();
     }
 }
