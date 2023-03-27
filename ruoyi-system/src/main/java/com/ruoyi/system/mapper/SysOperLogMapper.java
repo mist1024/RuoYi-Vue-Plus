d4ebbfde9cf2f934f7dfc672b66d1d8bb3e8b7dc
bleachtred <bleachtred@163.com>
2023-03-27 16:55:29 +0800

    update 优化BaseMapperPlus接口，去除接口参数Mapper泛型

--- ruoyi-system/src/main/java/com/ruoyi/system/mapper/SysOperLogMapper.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/mapper/SysOperLogMapper.java
@@ -8,6 +8,6 @@ import com.ruoyi.system.domain.SysOperLog;
  *
  * @author Lion Li
  */
-public interface SysOperLogMapper extends BaseMapperPlus<SysOperLogMapper, SysOperLog, SysOperLog> {
+public interface SysOperLogMapper extends BaseMapperPlus<SysOperLog, SysOperLog> {
 
 }
