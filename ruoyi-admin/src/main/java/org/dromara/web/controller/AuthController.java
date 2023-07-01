d05c641968f104f91db3f8161515c4eaff14111a
thiszhc <2029364173@qq.com>
2023-07-01 15:35:07 +0800

    统一客户端，优化授权流程

--- ruoyi-admin/src/main/java/org/dromara/web/controller/AuthController.java
+++ ruoyi-admin/src/main/java/org/dromara/web/controller/AuthController.java
@@ -1,12 +1,12 @@
 package org.dromara.web.controller;
 
 import cn.dev33.satoken.annotation.SaIgnore;
+import cn.dev33.satoken.stp.StpUtil;
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.util.ObjectUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import me.zhyd.oauth.model.AuthCallback;
 import me.zhyd.oauth.model.AuthResponse;
 import me.zhyd.oauth.model.AuthUser;
 import me.zhyd.oauth.request.AuthRequest;
@@ -87,7 +87,7 @@ public class AuthController {
     }
 
     /**
-     * 认证授权
+     * 第三方登录请求
      *
      * @param source 登录来源
      * @return 结果
@@ -98,35 +98,46 @@ public class AuthController {
         if (ObjectUtil.isNull(obj)) {
             return R.fail(source + "平台账号暂不支持");
         }
-        AuthRequest authRequest = SocialUtils.getAuthRequest(source,
-            obj.getClientId(),
-            obj.getClientSecret(),
-            obj.getRedirectUri());
+        AuthRequest authRequest = SocialUtils.getAuthRequest(source, socialProperties);
         String authorizeUrl = authRequest.authorize(AuthStateUtils.createState());
         return R.ok(authorizeUrl);
     }
 
     /**
      * 第三方登录回调业务处理
-     *
-     * @param source   登录来源
-     * @param callback 授权响应实体
+     *  绑定授权
+     * @param loginBody
      * @return 结果
      */
     @SuppressWarnings("unchecked")
-    @GetMapping("/social-login")
-    public R<String> socialLogin(String source, AuthCallback callback) {
-        SocialLoginConfigProperties obj = socialProperties.getType().get(source);
-        if (ObjectUtil.isNull(obj)) {
-            return R.fail(source + "平台账号暂不支持");
+    @PostMapping("/social-login")
+    public R<LoginVo> socialLogin(@RequestBody LoginBody loginBody) {
+        // 授权类型和客户端id
+        String clientId = loginBody.getClientId();
+        String grantType = loginBody.getGrantType();
+        SysClient client = clientService.queryByClientId(clientId);
+        // 查询不到 client 或 client 内不包含 grantType
+        if (ObjectUtil.isNull(client) || !StringUtils.contains(client.getGrantType(), grantType)) {
+            log.info("客户端id: {} 认证类型：{} 异常!.", clientId, grantType);
+            return R.fail(MessageUtils.message("auth.grant.type.error"));
         }
-        AuthRequest authRequest = SocialUtils.getAuthRequest(source,
-            obj.getClientId(),
-            obj.getClientSecret(),
-            obj.getRedirectUri());
-        AuthResponse<AuthUser> response = authRequest.login(callback);
-        return loginService.socialLogin(source, response);
+        // 校验租户
+        loginService.checkTenant(loginBody.getTenantId());
+        //判断用户登录状态，如果已经登录那么就默认为注册，如果没有登录那么默认登录
+        if (!StpUtil.isLogin()) {
+            return R.ok(IAuthStrategy.login(loginBody, client));
+        }else {
+            // 获取第三方登录信息
+            AuthResponse<AuthUser> response = SocialUtils.loginAuth(loginBody, socialProperties);
+            AuthUser authUserData = response.getData();
+            // 判断授权响应是否成功
+            if (!response.ok()) {
+                return R.fail(response.getMsg());
+            }
+            return loginService.sociaRegister(authUserData);
+        }
     }
+
 
     /**
      * 取消授权
