ea67ae1422344595ea02c683ef45ca19aeb8fd81
行者孤 <fxg885@163.com>
2023-08-01 02:37:17 +0000

    修复注册页面无法显示验证码的问题
    Signed-off-by: 行者孤 <fxg885@163.com>

--- ruoyi-ui-vue3/src/views/register.vue
+++ ruoyi-ui-vue3/src/views/register.vue
@@ -143,10 +143,10 @@ function handleRegister() {
 
 function getCode() {
   getCodeImg().then(res => {
-    captchaEnabled.value = res.captchaEnabled === undefined ? true : res.captchaEnabled;
+    captchaEnabled.value = res.data.captchaEnabled === undefined ? true : res.data.captchaEnabled;
     if (captchaEnabled.value) {
-      codeUrl.value = "data:image/gif;base64," + res.img;
-      registerForm.value.uuid = res.uuid;
+      codeUrl.value = "data:image/gif;base64," + res.data.img;
+      registerForm.value.uuid = res.data.uuid;
     }
   });
 }
