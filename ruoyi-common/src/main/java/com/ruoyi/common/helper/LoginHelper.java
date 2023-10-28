6e715d461a2f8d730a3c60208dd824e78430f578
Bozhu12 <1173930245@qq.com>
2023-10-28 11:15:45 +0800

    update 剔除不常用的方法

--- ruoyi-common/src/main/java/com/ruoyi/common/helper/LoginHelper.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/helper/LoginHelper.java
@@ -127,13 +127,6 @@ public class LoginHelper {
     }
 
     /**
-     * 获取用户部门名称
-     */
-    public static String getDeptName() {
-        return getLoginUser().getDeptName();
-    }
-
-    /**
      * 获取用户类型
      */
     public static UserType getUserType() {
