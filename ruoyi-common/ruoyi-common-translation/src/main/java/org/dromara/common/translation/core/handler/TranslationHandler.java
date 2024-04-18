73329e545af1904eed900ff48957192b653daf4b
Liang <545073804@qq.com>
2024-04-18 11:29:55 +0800

    update 翻译功能支持多值映射

--- ruoyi-common/ruoyi-common-translation/src/main/java/org/dromara/common/translation/core/handler/TranslationHandler.java
+++ ruoyi-common/ruoyi-common-translation/src/main/java/org/dromara/common/translation/core/handler/TranslationHandler.java
@@ -1,5 +1,7 @@
 package org.dromara.common.translation.core.handler;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.lang.Dict;
 import cn.hutool.core.util.ObjectUtil;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.BeanProperty;
@@ -14,6 +16,7 @@ import org.dromara.common.translation.core.TranslationInterface;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
@@ -38,9 +41,21 @@ public class TranslationHandler extends JsonSerializer<Object> implements Contex
         TranslationInterface<?> trans = TRANSLATION_MAPPER.get(translation.type());
         if (ObjectUtil.isNotNull(trans)) {
             // 如果映射字段不为空 则取映射字段的值
-            if (StringUtils.isNotBlank(translation.mapper())) {
-                value = ReflectUtils.invokeGetter(gen.getCurrentValue(), translation.mapper());
+            List<String> filterMappers = filterMappers();
+            if (CollUtil.isNotEmpty(filterMappers)) {
+                // 适配原有的单值映射
+                if (filterMappers.size() == 1) {
+                    value = ReflectUtils.invokeGetter(gen.getCurrentValue(), CollUtil.getFirst(filterMappers));
+                } else {
+                    // 多值映射
+                    Dict values = Dict.create();
+                    for (String mapper : filterMappers) {
+                        values.put(mapper, ReflectUtils.invokeGetter(gen.getCurrentValue(), mapper));
+                    }
+                    value = values;
+                }
             }
+
             // 如果为 null 直接写出
             if (ObjectUtil.isNull(value)) {
                 gen.writeNull();
@@ -51,6 +66,16 @@ public class TranslationHandler extends JsonSerializer<Object> implements Contex
         } else {
             gen.writeObject(value);
         }
+    }
+
+    /**
+     * 处理映射字段列表
+     */
+    private List<String> filterMappers() {
+        return CollUtil.newHashSet(translation.mapper())
+            .stream()
+            .filter(StringUtils::isNotBlank)
+            .toList();
     }
 
     @Override
