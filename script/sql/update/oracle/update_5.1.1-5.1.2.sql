dfe75f4f287316d8c3fcc6073d95d74b125598db
AprilWind <2100166581@qq.com>
2024-04-24 13:46:05 +0800

    add 新增update_5.1.2-5.2.0.sql

--- script/sql/update/oracle/update_5.1.1-5.1.2.sql
+++ script/sql/update/oracle/update_5.1.1-5.1.2.sql
@@ -4,6 +4,3 @@ insert into sys_menu values('1621', '配置添加', '118', '6', '#', '', '', 1, 
 insert into sys_menu values('1622', '配置编辑', '118', '6', '#', '', '', 1, 0, 'F', '0', '0', 'system:ossConfig:edit',   '#', 103, 1, sysdate, null, null, '');
 insert into sys_menu values('1623', '配置删除', '118', '6', '#', '', '', 1, 0, 'F', '0', '0', 'system:ossConfig:remove', '#', 103, 1, sysdate, null, null, '');
 
-ALTER TABLE sys_dept ADD dept_category VARCHAR2(100) DEFAULT NULL COMMENT '部门类别编码';
-ALTER TABLE sys_post ADD dept_id NUMBER(20) NOT NULL COMMENT '部门id', ADD post_category VARCHAR2(100) DEFAULT NULL COMMENT '岗位类别编码';
-UPDATE sys_post SET dept_id = 100;
--- script/sql/update/postgres/update_5.1.1-5.1.2.sql
+++ script/sql/update/postgres/update_5.1.1-5.1.2.sql
@@ -3,10 +3,3 @@ insert into sys_menu values('1620', '配置列表', '118', '5', '#', '', '', '1'
 insert into sys_menu values('1621', '配置添加', '118', '6', '#', '', '', '1', '0', 'F', '0', '0', 'system:ossConfig:add',    '#', 103, 1, now(), null, null, '');
 insert into sys_menu values('1622', '配置编辑', '118', '6', '#', '', '', '1', '0', 'F', '0', '0', 'system:ossConfig:edit',   '#', 103, 1, now(), null, null, '');
 insert into sys_menu values('1623', '配置删除', '118', '6', '#', '', '', '1', '0', 'F', '0', '0', 'system:ossConfig:remove', '#', 103, 1, now(), null, null, '');
-
-ALTER TABLE sys_dept
-    ADD COLUMN dept_category VARCHAR(100) DEFAULT NULL COMMENT '部门类别编码';
-ALTER TABLE sys_post
-    ADD COLUMN dept_id BIGINT NOT NULL COMMENT '部门id',
-    ADD COLUMN post_category VARCHAR(100) DEFAULT NULL COMMENT '岗位类别编码';
-UPDATE sys_post SET dept_id = 100;
